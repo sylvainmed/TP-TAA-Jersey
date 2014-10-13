55ca06016343a56c5f5b74358de33842bd2b5648
smed <medard.sylvain@gmail.com>
2014-10-13 16:40:05 +0200

    update

--- src/main/java/server/EventRessource.java
+++ src/main/java/server/EventRessource.java
@@ -2,40 +2,41 @@ package server;
 
 import java.util.Collection;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import shared.Event;
+import shared.EventList;
 import shared.JpaTest;
 import shared.Passager;
 
 import java.util.List;
 
-@Path("/events")
+@Path("/event")
 public class EventRessource {
 	
-	@POST
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public void addEvent(String name, String location){
-	System.out.println("Adds an event");
-	}
-	
-	@POST
-	@Path("/participer")
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	public void participe(int idEvent, Passager passager){
-	System.out.println("Inscrit un passager à un event");
+	public EventRessource() {
+		// TODO Auto-generated constructor stub
 	}
 	
 	@GET
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public List<Event> getEvents(){
-	System.out.println("Returns the events list");
-	return JpaTest.getEvents();
+		return EventList.getInstance().getEvents();
+	}
+	
+	@POST
+	@Path("/new")
+	@Consumes({MediaType.APPLICATION_JSON})
+	public void participer (int idEvent, Passager passager){
+		EventList.getInstance().participe(passager, idEvent);
+		System.out.println("Passager ajouté à un event");
 	}
 
 }
--- src/main/java/shared/JpaTest.java
+++ src/main/java/shared/JpaTest.java
@@ -1,98 +1,34 @@
 package shared;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.NoResultException;
-import javax.persistence.Persistence;
-
-import java.util.List;
 
 public class JpaTest {
+	
+	EventList eventlist;
+	
+	
+	public JpaTest(EventList eventlist) {
+		this.eventlist = eventlist;
 
-	private EntityManager manager;
-	EntityTransaction tx;
-	
-	public JpaTest(EntityManager manager) {
-		this.manager = manager;
-		this.tx= manager.getTransaction();
-
 	}
 	
-	public void addVoiture(int places, int idevent){
-		tx.begin();
-		Voiture car = new Voiture();
-		Event event = getEvent(idevent);
-		car.setPlaces(places);
-		car.setEvent(event);
-		manager.persist(car);
-		tx.commit();
+	public EventList getEventlist() {
+		return eventlist;
 	}
-	
-	public Passager addPassager(int idEvent, String name){
-		tx.begin();
-		Event event = getEvent(idEvent);
-		Passager passager = new Passager();
-		passager.setNom(name);
-		passager.setEvent(event);
-		manager.persist(passager);
-		tx.commit();
-		return passager;
-		}
-	
-	public void addEvent(String place, String date, String heure){
-		Event event = new Event();
-		event.setDate(date);
-		event.setHeure(heure);
-		event.setPlace(place);
-		tx.begin();
-		manager.persist(event);
-		tx.commit();
-			
-		
-	}
-	
-	public List<Event> getEvents(){
-		return manager.createQuery("select e FROM Event e", Event.class).getResultList();
-	}
-	
-	public Event getEvent(int id){
-		Event event = (Event) manager.find(Event.class, 1);
-		return(event);
-	}
-	
-	public void participe (Passager passager, int idevent){
-		tx.begin();
-		Event event = (Event) manager.createQuery("FROM Event WHERE id="+idevent).getSingleResult();
-		passager.setEvent(event);
-		event.getPassagers().add(passager);
-		
-		tx.commit();
-	}
-	
-	public void ajouteDansVoiture(Passager passager, int idvoiture){
-		tx.begin();
-		Voiture voiture = (Voiture) manager.createQuery("FROM Car WHERE id="+idvoiture).getSingleResult();
-		passager.setVoiture(voiture);
-		voiture.getPassagers().add(passager);
-		voiture.decPlaces();
-		tx.commit();
-	}
-	
 	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
-		EntityManager manager = factory.createEntityManager();
-		JpaTest test = new JpaTest(manager);
+//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
+//		EntityManager manager = factory.createEntityManager();
+//		JpaTest test = new JpaTest(manager);
+		EventList eventlist = new EventList();
 
 
 		try {
-			test.addVoiture(3, 5);
-			test.addVoiture(2, 6);
-			test.addEvent("5", "23/02/1998", "12:25");
+			eventlist.addVoiture(3, 5);
+			eventlist.addVoiture(2, 6);
+			eventlist.addEvent("5", "23/02/1998", "12:25");
 					
 		} catch (Exception e) {
 			e.printStackTrace();
